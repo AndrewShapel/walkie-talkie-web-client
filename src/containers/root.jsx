1be75ed9f2d0cb7c2b831430148a20288545b434
Andrew Shapel <andrewshapel@users.noreply.github.com>
2017-03-22 16:52:39 +0300

    feat(wt.chat): add css-loader and style-loader to project dependencies

--- src/containers/root.jsx
+++ src/containers/root.jsx
@@ -3,18 +3,22 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import routes from '../constants/routes/routes';
 
+import rootClassNames from '../assets/css/blocks/root/root.css';
+
 import About from '../containers/about';
 import Home from '../containers/home';
 import NotFound from '../containers/not-found';
 
 const Root = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path={routes.home.url} component={Home} />
-      <Route path={routes.about.url} component={About} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <div className={rootClassNames.root}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={routes.home.url} component={Home} />
+        <Route path={routes.about.url} component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  </div>
 );
 
 export default Root;
