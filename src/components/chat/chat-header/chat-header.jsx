03ce522dcb282e2430febaa3c727b0d260691dd1
Andrew Shapel <andrewshapel@users.noreply.github.com>
2017-03-22 20:49:24 +0300

    feat(wt.chat): move header styles to BEM class convention

--- src/components/chat/chat-header/chat-header.jsx
+++ src/components/chat/chat-header/chat-header.jsx
@@ -3,23 +3,23 @@ import React from 'react';
 import chatHeaderClassNames from '../../../assets/css/blocks/chat/chat-header/chat-header.css';
 
 export default () => (
-  <div className={chatHeaderClassNames.container}>
-    <div className={chatHeaderClassNames.userContainer}>
+  <div className={chatHeaderClassNames.chatHeader}>
+    <div className={chatHeaderClassNames.chatHeader__user}>
       <div className={chatHeaderClassNames.avatar} />
-      <div className={chatHeaderClassNames.userInformation}>
-        <span className={chatHeaderClassNames.userName}>Beerfest</span>
-        <span className={chatHeaderClassNames.userStatus}>Online</span>
+      <div className={chatHeaderClassNames.chatHeader__userInformation}>
+        <span className={chatHeaderClassNames.chatHeader__userName}>Beerfest</span>
+        <span className={chatHeaderClassNames.chatHeader__userStatus}>Online</span>
       </div>
     </div>
-    <div className={chatHeaderClassNames.searchContainer}>
+    <div className={chatHeaderClassNames.chatHeader__search}>
       <div className={chatHeaderClassNames.search}>
-        <input className={chatHeaderClassNames.searchInput} type="text" />
-        <object className={chatHeaderClassNames.searchIcon} data="assets/icons/svg/search.svg" type="iamge/svg+xml" />
+        <input className={chatHeaderClassNames.search__searchInput} type="text" />
+        <object className={chatHeaderClassNames.search__searchIcon} data="assets/icons/svg/search.svg" type="iamge/svg+xml" />
       </div>
     </div>
-    <div className={chatHeaderClassNames.actionsContainer}>
-      <div className={chatHeaderClassNames.action}>
-        <object className={chatHeaderClassNames.actionIcon} data="assets/icons/svg/video-camera.svg" type="image/svg+xml" />
+    <div className={chatHeaderClassNames.actions}>
+      <div className={chatHeaderClassNames.actions__action}>
+        <object className={chatHeaderClassNames.actions__actionIcon} data="assets/icons/svg/video-camera.svg" type="image/svg+xml" />
       </div>
       <div className={chatHeaderClassNames.action}>
         <object className={chatHeaderClassNames.actionIcon} data="assets/icons/svg/phone.svg" type="image/svg+xml" />
