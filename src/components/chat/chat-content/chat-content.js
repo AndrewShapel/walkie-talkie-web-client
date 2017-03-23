13c3ab99986020becf4a3bcf01b8e74b5b485637
Andrew Shapel <andrewshapel@users.noreply.github.com>
2017-03-23 15:31:00 +0300

    feat(wt.chat): add messages and message components with styles

--- src/components/chat/chat-content/chat-content.js
+++ src/components/chat/chat-content/chat-content.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import classnames from 'classnames';
 
+import ChatMessages from '../chat-messages/chat-messages';
+
 import chatContentClassNames from '../../../assets/css/blocks/chat/chat-content/chat-content.css';
 
 const ChatContent = ({ className }) => {
   const chatContentClassName = classnames(chatContentClassNames['chat-content'], className);
 
   return (
-    <div className={chatContentClassName}>24</div>
+    <div className={chatContentClassName}>
+      <ChatMessages />
+    </div>
   );
 };
 
 ChatContent.defaultProps = {
-  className: null,
+  className: '',
 };
 
 ChatContent.propTypes = {
--- src/components/user-avatar/user-avatar.jsx
+++ src/components/user-avatar/user-avatar.jsx
@@ -5,24 +5,41 @@ import { USER_STATUS } from '../../constants/common';
 
 import userAvatarClassNames from '../../assets/css/blocks/user-avatar/user-avatar.css';
 
-const UserAvatar = ({ userStatus }) => {
-  const statusIconClassName = classnames(userAvatarClassNames['user-avatar__status-icon'], {
-    [userAvatarClassNames['user-avatar__status-icon_online']]: userStatus === USER_STATUS.ONLINE,
-    [userAvatarClassNames['user-avatar__status-icon_offline']]: userStatus === USER_STATUS.OFFLINE,
-    [userAvatarClassNames['user-avatar__status-icon_do_not_disturb']]: userStatus === USER_STATUS.DO_NOT_DISTURB,
-  });
+export default class UserAvatar extends React.PureComponent {
+  /**
+   * @param {String} userStatus
+   * @returns {Object}
+   */
+  static renderStatus(userStatus) {
+    const statusIconClassName = classnames(userAvatarClassNames['user-avatar__status-icon'], {
+      [userAvatarClassNames['user-avatar__status-icon_online']]: userStatus === USER_STATUS.ONLINE,
+      [userAvatarClassNames['user-avatar__status-icon_offline']]: userStatus === USER_STATUS.OFFLINE,
+      [userAvatarClassNames['user-avatar__status-icon_do_not_disturb']]: userStatus === USER_STATUS.DO_NOT_DISTURB,
+    });
 
-  return (
-    <div className={userAvatarClassNames['user-avatar']}>
+    return (
       <div className={userAvatarClassNames['user-avatar__status']}>
         <div className={statusIconClassName} />
       </div>
-    </div>
-  );
+    );
+  }
+
+  render() {
+    const { userStatus } = this.props;
+
+    return (
+      <div className={userAvatarClassNames['user-avatar']}>
+        { userStatus && UserAvatar.renderStatus(userStatus) }
+      </div>
+    );
+  }
+}
+
+UserAvatar.defaultProps = {
+  userStatus: null,
 };
 
 UserAvatar.propTypes = {
-  userStatus: React.PropTypes.string.isRequired,
+  userStatus: React.PropTypes.string,
 };
 
-export default UserAvatar;
