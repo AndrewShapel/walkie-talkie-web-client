fd93fd065f5fe501c18b3c590aba983296728ee8
Andrew Shapel <andrewshapel@users.noreply.github.com>
2017-04-07 13:54:22 +0300

    feat(wt.routes-improvement): add redirect if "verification" path is exact

--- src/containers/user-verification.jsx
+++ src/containers/user-verification.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import routes from '../constants/routes/routes';
+
 import Verification from '../components/verification/verification';
 
 import verificationClassNames from '../assets/css/containers/user-verification/user-verification.css';
@@ -8,14 +10,28 @@ export default class VerificationContainer extends React.Component {
 
   static propTypes = {
     match: React.PropTypes.object,
-  }
+    history: React.PropTypes.object,
+  };
 
   static defaultProps = {
     match: {},
+    history: {},
+  };
+
+  componentWillMount() {
+    const { match, history } = this.props;
+
+    if (match.isExact) {
+      const { location, replace } = history;
+
+      const redirectToPathName = `${location.pathname}${routes.userVerification.url.signin}`;
+      replace(redirectToPathName);
+    }
   }
 
   render() {
     const { match } = this.props;
+
     return (
       <div className={verificationClassNames['user-verification']}>
         <Verification match={match} className={verificationClassNames['user-verification__content']} />
