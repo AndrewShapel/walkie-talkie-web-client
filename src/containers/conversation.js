bf3f78e49eaaced2047a6d2859862ae950e94777
Andrew Shapel <andrewshapel@users.noreply.github.com>
2017-04-16 18:12:30 +0300

    feat(wt.verification-implement): add "permit" component to restrict access to components

--- src/containers/conversation.js
+++ src/containers/conversation.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import { USER_PERMISSION } from '../constants/user';
+
+import Permit from '../components/permit/permit';
 import Chat from '../components/chat/chat';
 import Panel from '../components/panel/panel';
 
@@ -21,10 +24,12 @@ export default class Conversation extends React.Component {
 
   render() {
     return (
-      <div className={chatClassNames.conversation}>
-        <Panel className={chatClassNames.conversation__panel} />
-        <Chat className={chatClassNames.conversation__chat} />
-      </div>
+      <Permit permission={USER_PERMISSION.BASIC}>
+        <div className={chatClassNames.conversation}>
+          <Panel className={chatClassNames.conversation__panel} />
+          <Chat className={chatClassNames.conversation__chat} />
+        </div>
+      </Permit>
     );
   }
 }
