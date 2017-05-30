9f00b6a4ba26f64495c918fdc15e01db22042fb1
Andrew Shapel <andrewshapel@users.noreply.github.com>
2017-05-30 13:14:09 +0300

    feat(wt-conversations-implement): add "conversations" model, action creators and reducer

--- src/containers/conversation.jsx
+++ src/containers/conversation.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import routes from '../constants/routes/routes';
 import { USER_PERMISSION } from '../constants/user';
+
+import { setActiveId, resetActiveId } from '../action-types/conversations';
 
 import Permit from '../components/permit/permit';
 import Chat from '../components/chat/chat';
@@ -9,18 +14,55 @@ import Panel from '../components/panel/panel';
 
 import chatClassNames from '../assets/css/containers/conversation/conversation.css';
 
+/**
+ * @param {Object} Conversations
+ * @returns {Object}
+ */
+const mapStateToProps = ({ Conversations }) => ({
+  activeConversationId: Conversations.getActiveId(),
+});
+
+/**
+ * @param {Function} dispatch
+ * @returns {Object}
+ */
+const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    setActiveConversationIdAction: setActiveId,
+    resetActiveConversationIdAction: resetActiveId,
+  }, dispatch)
+);
+
+@connect(mapStateToProps, mapDispatchToProps)
 export default class Conversation extends React.Component {
 
   static propTypes = {
+    match: React.PropTypes.object,
     history: React.PropTypes.object,
+    activeConversationId: React.PropTypes.string,
+    setActiveConversationIdAction: React.PropTypes.func,
+    resetActiveConversationIdAction: React.PropTypes.func,
   };
 
   static defaultProps = {
+    match: {},
     history: {},
+    activeConversationId: '',
+    setActiveConversationIdAction: null,
+    resetActiveConversationIdAction: null,
   };
 
   componentWillMount() {
-    // Set conversation id to redux store
+    const { match, activeConversationId, setActiveConversationIdAction, resetActiveConversationIdAction } = this.props;
+
+    if (match) {
+      const conversationId = match.params.id;
+      if (conversationId && conversationId !== activeConversationId) {
+        setActiveConversationIdAction(conversationId);
+      } else {
+        resetActiveConversationIdAction();
+      }
+    }
   }
 
   render() {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,2 +1,3 @@
 export { default as Users } from './users';
 export { default as Messages } from './messages';
+export { default as Conversations } from './conversations';
