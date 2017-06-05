a52990907897491135c2481b485761b6902753fb
Andrew Shapel <andrewshapel@users.noreply.github.com>
2017-06-05 18:33:51 +0300

    feat(wt-graphql-integration): add buttons to accept or decline friend request in "panel-requests-item" component

--- src/components/panel/panel-requests/panel-requests-item/panel-requests-item.jsx
+++ src/components/panel/panel-requests/panel-requests-item/panel-requests-item.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import classnames from 'classnames';
 
+import { ICONS } from '../../../../constants/icons';
+
+import Svg from '../../../svg/svg';
 import PanelContentUser from '../../panel-content/panel-content-user/panel-content-user';
 
 import panelRequestsClassNames from './panel-requests-item.css';
@@ -15,9 +19,23 @@ export default class PanelRequestsItem extends React.PureComponent {
   };
 
   render() {
+    const iconClassName = panelRequestsClassNames['panel-requests-item__icon'];
+    const iconBlockedClassName = classnames(iconClassName, panelRequestsClassNames['panel-requests-item__icon_block']);
+    const iconAcceptClassName = classnames(iconClassName, panelRequestsClassNames['panel-requests-item__icon_accept']);
+
     return (
       <li className={panelRequestsClassNames['panel-requests-item']}>
         <PanelContentUser />
+        <div className={panelRequestsClassNames['panel-requests-item__icons']}>
+          <Svg
+            className={iconBlockedClassName}
+            icon={ICONS.BLOCKED}
+          />
+          <Svg
+            className={iconAcceptClassName}
+            icon={ICONS.USER_CHECK}
+          />
+        </div>
       </li>
     );
   }
